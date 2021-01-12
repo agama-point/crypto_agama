994e9aeb7cdce8e77dad7df0d6335fcc61dc3f6f
Honza S. Copak <bonum.comunae@volny.cz>
2021-01-12 09:40:17 +0100

    clean up

--- seed-find-last.py
+++ seed-find-last.py
@@ -11,7 +11,6 @@ bip39 = seedWords()
 words_book = "army van defense carry jealous true garbage claim echo media make crunch"
 words_tbtc = "major easy ignore body rule stay gorilla eager arch actor scan thank"
 words_test = "employ blouse total detect move attitude trophy space crystal size green naive"
-test_awsom = "quick brown fox ..."
 
 words_test11 = "major easy ignore body rule stay gorilla eager arch actor scan "
 
