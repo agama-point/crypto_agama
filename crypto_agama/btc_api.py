6e9d82e2f6e5d9ba62c4f69334af532bc349bda5
Honza S. Copak <bonum.comunae@volny.cz>
2021-01-26 12:46:49 +0100

    added fnc

--- crypto_agama/btc_api.py
+++ crypto_agama/btc_api.py
@@ -4,9 +4,9 @@
 import requests, json
 
 url_btcusd = "https://api.coinpaprika.com/v1/tickers/btc-bitcoin"
-url_btct = "https://chain.so/api/v2/get_tx_received/BTCTEST/$addrBTCT"
 url_mempool_fees = "https://mempool.space/api/v1/fees/recommended"
-
+url_get_tx_received = "https://chain.so/api/v2/get_tx_received"
+url_get_address_balance = "https://chain.so/api/v2/get_address_balance"
 
 # note: 10-20 sec. pause is required
 def bitcoin_usd():
@@ -26,9 +26,54 @@ def btc_mempool_fees():
     return fastestFee, halfHourFee, hourFee
 
 
-def btct_info():
-    import requests, json
-    res = requests.get(url_btct)
-    print(res)
-    btcti = res.json()['quotes']["USD"]["price"]
-    return float(btcti)
+def addr_info(url_coin,addr, debug=False):
+    url_tx = url_get_tx_received + url_coin + addr
+    url_adrbal = url_get_address_balance + url_coin + addr
+    if debug: 
+        print(url_tx)
+        print(url_adrbal)
+
+    res_num_txs = 0
+    res_fin_bal = 0
+
+    try:
+        res = requests.get(url_tx)
+        #print(res)
+        res_json =  res.json()
+        #print(res_json)
+        res_stat =res_json['status']
+        if res_stat == "success":
+            #data': {'network': 'BTCTEST',
+            res_netw = res_json['data']['network']
+            res_txs = res_json['data']['txs']
+            res_num_txs = len(res_txs)
+            if debug: print("network: ", res_netw, " | txs: ", res_num_txs)
+        else:
+            if debug: print("status1: FALSE")
+    except:
+        print("url_get_tx_received.err")
+
+    try:
+
+        res = requests.get(url_adrbal)
+        # print(res)
+        res_json =  res.json()
+        res_stat =res_json['status']
+        if res_stat == "success":
+            #data': {'network': 'BTCTEST',
+            res_cb = res_json['data']['confirmed_balance']
+            res_fin_bal = res_cb
+            res_ucb = res_json['data']['unconfirmed_balance']
+            if debug: print("balance: ", res_cb, res_ucb)
+
+        else:
+            if debug: print("status2: FALSE")
+ 
+
+    except:
+        print("url_get_tx_received.err")
+
+    return res_num_txs, res_fin_bal
+
+    #btcti = res.json()['quotes']["USD"]["price"]
+    #return float(btcti)
